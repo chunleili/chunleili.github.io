c97e7b000cc545548aaed74ed0714b8ba9ae3f47
chunleili <li_cl@foxmail.com>
2022-10-02 21:34:01 +0800

    Create docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/chunleili/chunleili.github.io/tree/master/docs',
+            'https://github.com/chunleili/chunleili.github.io/tree/master',
         },
         blog: {
           showReadingTime: true,
